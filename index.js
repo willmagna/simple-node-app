0957d693ab339099e9e2a9b80538115132950c2c
William Magnabosco <williamlodea@gmail.com>
2024-08-08 08:29:53 -0400

    a

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/hello-world", (req, res) => {
+  res.send("Hello World!2");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
